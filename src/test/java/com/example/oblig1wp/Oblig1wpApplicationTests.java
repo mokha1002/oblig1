87d0eb75a67a8f24395772b0d22c884dce32fdd0
Ali Khan <mokha1002@oslomet.no>
2024-02-13 13:52:18 +0100

    opprettelse og oppdatering av READMe-fil

---
+++ src/test/java/com/example/oblig1wp/Oblig1wpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1wp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1wpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
