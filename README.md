ccc7adcfd3966d6caa0c5d55f5365625ece6796f
Ali Khan <mokha1002@oslomet.no>
2024-02-18 20:38:32 +0100

    readme og kommentarer i js

--- README.md
+++ README.md
@@ -3,3 +3,33 @@
 OsloMet brukernavn: mokha1002
 
 GitHub brukernavn: alikhannpc
+
+URL:
+
+# Billettbestillingsside
+
+Denne nettsiden lar brukerne bestille billetter til filmer. 
+Når brukerne fyller ut skjemaet med informasjon om filmvalg, 
+antall billetter og personlig informasjon, kan de lagre 
+bestillingen. Her er en kort oversikt over funksjonaliteten:
+
+## Funksjoner:
+
+1. **Billettbestilling:**
+    - Brukerne kan fylle ut et skjema med informasjon om hvilken film de ønsker å se, antall billetter, samt personlig informasjon som fornavn, etternavn, telefonnummer og e-postadresse.
+    - Før bestillingen lagres, valideres inputfeltene for å sikre at de er riktig utfylt. Hvis det er feil eller mangler, vises feilmeldinger.
+    - Når all informasjon er gyldig, kan brukerne lagre bestillingen, og informasjonen lagres internt i nettleseren.
+
+2. **Visning av billetter:**
+    - Etter at brukeren har lagret en eller flere bestillinger, vises informasjonen om billettene på nettsiden. Brukerne kan se hvilken film de har bestilt billetter til, antall billetter, samt deres personlige informasjon.
+
+3. **Slett bestillinger:**
+    - Det er også mulighet til å slette alle lagrede bestillinger. Dette tømmer arrayet som holder informasjonen om billettene og fjerner dem fra visningen på nettsiden.
+
+## Hvordan bruke:
+
+1. Åpne `index.html` i en nettleser.
+2. Fyll ut skjemaet med informasjonen om filmen du ønsker å se og din personlige informasjon.
+3. Klikk på "Kjøp billett" for å lagre bestillingen.
+4. Bestillingen vil vises på nettsiden, og du kan gjenta prosessen for å legge til flere bestillinger.
+5. For å fjerne alle bestillinger, klikk på "Slett alle billetter".
--- src/main/resources/static/javascript.js
+++ src/main/resources/static/javascript.js
@@ -1,6 +1,9 @@
+// En tom array for å lagre billettinformasjon
 let billettArray = [];
 
+// Funksjon for å lagre billettinformasjon når brukeren trykker på en "Kjøp billett" på nettsiden
 function billettLagring() {
+    // Henter verdier fra inputfeltene i HTML
     let film = document.getElementById("bestilling").value;
     let antall = document.getElementById("antall").value;
     let fornavn = document.getElementById("fornavn").value;
@@ -8,21 +11,26 @@ function billettLagring() {
     let telefonnr = document.getElementById("telefonnr").value;
     let epost = document.getElementById("epost").value;
 
+    // Nullstill feilmeldinger på siden, funksjon for nullstilling lages lenger ned i scriptet
     nullstillFeilmeldinger();
 
+    // Valideringsregler for inputfeltene, av type boolean, som returnerer true eller false
     let antallValidering = antall !== "" && !isNaN(antall) && antall > 0;
     let fornavnValidering = fornavn !== "";
     let etternavnValidering = etternavn !== "";
     let telefonnrValidering = telefonnr !== "" && !isNaN(telefonnr) && telefonnr.length === 8;
-    let epostpattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    let epostpattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // epostvalidering fra: https://emaillistvalidation.com/blog/email-validation-in-javascript-using-regular-expressions-the-ultimate-guide/
     let epostValidering = epostpattern.test(epost);
 
-
+    // Hvis all input er gyldig, legg til i array, ellers vis feilmeldinger, vi kan gjøre dette på grunn av at de
+    // forskjellige valideringene returnerer en boolean, vi vil dermed kun gå videre og legge til i array, skrive ut og
+    // tømme inputfeltene hvis all input er riktig
     if (antallValidering && fornavnValidering && etternavnValidering && telefonnrValidering && epostValidering) {
         leggTilIArrayet();
         skrivUtBilletter();
         tommeInputfelt();
     } else {
+        // Viser feilmeldinger for hver valideringsregel som ikke er oppfylt
         if (antall === "") {
             document.getElementById("feilAntall").innerText = "Skriv inn antallet billetter";
         } else if (antallValidering === false) {
@@ -30,7 +38,6 @@ function billettLagring() {
         } else {
             document.getElementById("feilAntall").innerText = "";
         }
-
         if (fornavn === "") {
             document.getElementById("feilFornavn").innerText = "Skriv inn fornavnet";
         } else {
@@ -57,20 +64,16 @@ function billettLagring() {
         }
     }
 
+    // Funksjon for å nullstille feilmeldinger på siden
     function nullstillFeilmeldinger() {
         document.getElementById("feilAntall").innerText = "";
-        document.getElementById("feilAntall").style.color = "red";
         document.getElementById("feilFornavn").innerText = "";
-        document.getElementById("feilFornavn").style.color = "red";
         document.getElementById("feilEtternavn").innerText = "";
-        document.getElementById("feilEtternavn").style.color = "red";
         document.getElementById("feilTlf").innerText = "";
-        document.getElementById("feilTlf").style.color = "red";
         document.getElementById("feilEpost").innerText = "";
-        document.getElementById("feilEpost").style.color = "red";
     }
 
-
+    // Legger til bestilling i arrayet
     function leggTilIArrayet() {
         let nyBestilling = {
             film: film,
@@ -83,8 +86,9 @@ function billettLagring() {
         billettArray.push(nyBestilling)
     }
 
+    // Tømmer inputfeltene etter lagring av billett
     function tommeInputfelt() {
-        document.getElementById("bestilling").val;
+        document.getElementById("bestilling").value  = "";
         document.getElementById("antall").value = "";
         document.getElementById("fornavn").value = "";
         document.getElementById("etternavn").value = "";
@@ -92,28 +96,38 @@ function billettLagring() {
         document.getElementById("epost").value = "";
     }
 
+    // Skriver ut billetter basert på data i arrayet
     function skrivUtBilletter() {
         let utskrift = "";
         for (let i = 0; i < billettArray.length; i++) {
             let billett = billettArray[i];
-            utskrift += `Film: ${billett.film}, Antall: ${billett.antall}, Fornavn: ${billett.fornavn}, Etternavn: ${billett.etternavn}, Telefonnr: ${billett.telefonnr}, Epost: ${billett.epost}<br>`;
+            utskrift += "Film: " + billett.film + ", Antall: " + billett.antall + ", Fornavn: " + billett.fornavn + ", Etternavn: " + billett.etternavn + ", " + "Telefonnr: " + billett.telefonnr + ", Epost: " + billett.epost + "<br>";
         }
         document.getElementById("utskriftFilmArray").innerHTML = utskrift;
         console.log(utskrift)
     }
 }
 
+// Funksjon for å slette alle billettene fra arrayet, bruker splice for å slette innhold i arrayet
 function slettArray() {
+    // Tømmer hele arrayet
     billettArray.splice(0, billettArray.length);
 
+    // Funksjon for å skrive ut at arrayet er tomt, vi kunne også bare gjort slik at utskriftFilmArray-listen i HTML
+    // sin (document.getElementbyId(utskriftFilmarray).innerHTML = ""), slik at innholdet fjernes fra siden,
+    // men velger å skrive ut arrayet i en for-løkke selvom det er tomt, for å vise at arrayet er tomt
     function slettBilletter() {
         let utskriftSlettaArray = "";
         for (let i = 0; i < billettArray.length; i++) {
             let billett = billettArray[i];
-            utskrif += `Film: ${billett.film}, Antall: ${billett.antall}, Fornavn: ${billett.fornavn}, Etternavn: ${billett.etternavn}, Telefonnr: ${billett.telefonnr}, Epost: ${billett.epost}<br>`;
+            // Legger til hver billetts informasjon til en streng
+            utskriftSlettaArray += "Film: " + billett.film + ", Antall: " + billett.antall + ", Fornavn: " + billett.fornavn + ", Etternavn: " + billett.etternavn + ", Telefonnr: " + billett.telefonnr + ", Epost: " + billett.epost + "<br>";
+
         }
+        // Skriver ut strengen på nettsiden
         document.getElementById("utskriftFilmArray").innerHTML = utskriftSlettaArray;
         console.log(utskriftSlettaArray)
     }
+    // Kaller funksjonen for å skrive ut tomme billetter
     slettBilletter();
 }
